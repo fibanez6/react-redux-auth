38790ce263461d2d8a2e920c62b3e5940f4f9434
fibanez <fibanez@fibanez.com>
2018-02-27 10:52:40 +0000

    authentication wrapper

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# React Redux SimpleStarter
+# React Redux Auth
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+Higher Order Components
+
+[Advanced React-Redux tutorial](https://www.udemy.com/react-redux-tutorial) to display the weather forecast of a city.
+This project cover section 4 of the tutorial.
+
 
 ### IMPORTANT
 
 This project was cloned from [StephenGrider/ReduxSimpleStarter](https://github.com/StephenGrider/ReduxSimpleStarter)
 
+### Content
+* Authentication HOC
+* React Router setup
+* Action Creator Hookup
+* HOC Scaffold
 
 ### Installation
 
 ```
-> git clone https://github.com/fibanez6/react-redux-simpleStarter.git
-> cd react-redux-simpleStarter
+> git clone https://github.com/fibanez6/react-redux-auth.git
+> cd react-redux-auth
 > npm install
 > npm start
 ```
--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
 
+import requiredAuth from './components/require_authentication';
 import App from './components/app';
+import Resources from './components/resources';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory}>
+        <Router path="/" component={App}>
+            <Route path="resources" component={requiredAuth(Resources)} />
+        </Router>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
